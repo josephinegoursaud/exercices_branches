50af11be8b7662722c50f7c844a0f146d0f30ec4
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-06-03 10:15:31 +0200

    correction ex debug

--- synthaxe/debug.js
+++ synthaxe/debug.js
@@ -1,16 +1,33 @@
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de deux articles
-let a1 = 10
-let a2 = 20
+//let a1 = 10
+//et a2 = 20
 // Nous calculons le total
-const tot = a1 + a2
+//const tot = a1 + a2
 // Prix d'un troisieme article
-let a3 = "50"
+//let a3 = "50"
 // Nous ajoutons a3 au total
-tot = tot + a3
+//tot = tot + a3
 
-console.log("Le prix total avant reduction est de : ", tot)
-console.log("Le prix total après reduction est de : ", tot2)
+//console.log("Le prix total avant reduction est de : ", tot)
+
 
 //Nous appliquons 20% de reduction
+let reduction = total*0,2
+console.log("reduction, reduction")
+let finalPrice = total - reduction
+
+let a1 = 10
+let a2 = 20
+
+//const tot = a1 + a2
+//console.log ( "le prix total avant reduction est de:", tot)
+let tot = a1 + a2
+
+let a3 = "50"
+tot = tot + a3
 let tot2 = tot - tot*0,2
+console.log("le prix total avant reduction est de :", tot)
+console.log("le prix total après reduction est de : ", tot2)
+
+
