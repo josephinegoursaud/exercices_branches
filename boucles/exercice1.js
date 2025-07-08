ebb6ce70b2ad157bca1f3adc1197ee20e7671081
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-07-08 11:12:04 +0200

    exercice 1 boucles fait

---
+++ boucles/exercice1.js
@@ -0,0 +1,30 @@
+//Écris une boucle FOR qui affiche dans la console tous les nombres de 0 à 99.
+
+// for (let i = 0; i < 100; i++) { // affiche 0, puis 1, puis 2
+//   console.log((i))
+// }
+
+//Écris le même compteur avec une boucle WHILE.
+// let i = 0
+// while (i < 100) { // affiche 0, puis 1, puis 2
+//   console.log( i );
+//   i++;
+// }
+
+//Réflexion : Quel type de boucle te semble le plus approprié pour cette application ?
+//le boucle while
+
+//Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
+
+// let i = 50
+// while (i<100){
+//     console.log(i)
+//     i++
+// }
+
+//Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
+let i = 49
+while (i>0){
+    console.log(i)
+i--
+}
