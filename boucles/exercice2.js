c0a7d2442e2c463e8a8c3802501a34181aff7d0a
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-07-08 15:33:08 +0200

    exercice2 fait

---
+++ boucles/exercice2.js
@@ -0,0 +1,25 @@
+// Créer une variable appelé “message” de type string.
+
+// let letters = [ 'A', 'd', 'a', '-', 'T', 'e', 'c', 'h']
+ let message = ""
+
+// Créer une boucle qui va parcourir le tableau letters (choisis la boucle de ton choix)
+
+
+//  let i= 0
+//  while (i < letters.length){
+//     message += letters[i];  // Ajouter chaque lettre à message
+//      i++
+    
+//  }
+//  console.log(message)
+
+ let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l']
+
+ let i= 0
+ while (i < letters.length){
+    message += letters[i];  // Ajouter chaque lettre à message
+      i++
+ }
+ // Dans le code de la boucle, ajouter chaque élément du tableau letters à message.
+console.log(message)
