184bf9f8655c3724a42e970994202e8f49e71e27
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-07-09 09:36:01 +0200

    exercice3 fait

---
+++ boucles/exercice3.js
@@ -0,0 +1,18 @@
+//Demande à l'utilisateur (à l'aide de prompt) un nombre entre 0 et 10.
+//Affiche ensuite tous les nombres sous le nombre indiqué par l'utilisateur
+// (s'il rentre 4, on affichera 4, 3, 2, 1, 0).
+
+
+//let number= prompt("Veuillez entrer un nombre entre 0 et 10 :")
+//console.log(number)
+
+let i= 7
+ while (i !=-1) { 
+    // if (i===-1){
+    //     break
+    // }
+console.log( i);
+
+   i--;
+  
+}
