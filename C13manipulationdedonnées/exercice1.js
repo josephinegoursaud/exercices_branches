39a878ae784aa96e34005bf4bd2c0766ef4ec29c
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-07-15 10:33:30 +0200

    exercice1 fait

---
+++ C13manipulationdedonnées/exercice1.js
@@ -0,0 +1,15 @@
+const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega',
+ 'Misty Phoenix','Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+
+
+// console.log(candidates[0])
+// console.log(candidates[7])
+console.log(candidates)
+
+//  let list = document.getElementById("myList");
+            
+// for (i = 0; i < candidates.length; i++) {
+// let li = document.createElement('li');
+// li.innerText = candidates[i];
+// list.appendChild(li);
+// }
