6cc272c1dbf142ad1b6a303e82e0429516481d5f
josephinegoursaud <josephinegoursaud@hotmail.fr>
2025-07-16 15:50:28 +0200

    exo 1

--- C14fetch/exercice1.js
+++ C14fetch/exercice1.js
@@ -2,27 +2,42 @@
 // console.log(promise);
 
 async function fetchdata() {
-    const response = await fetch("https://codepassport.dev/api/offers")
+    const response = await fetch("https://www.codepassport.dev/api/offers")
     const data = await response.json()
-    console.log(data)
-}
-fetchdata()
-   const title= document.createElement('h2');
-   titleetdescription.appendChild(title)
 
-   title.innerHTML=data[i].titre
+    const titleetdescription = document.getElementById("titleetdescription")
 
-const paragraphe=document.createElement('p')
-titleetdescription.appendChild(paragraphe)
-title.innerHTML=data[i].paragraphe
+    for (let i = 0; i < data.length; i++) {
   
-for (let i = 0; i < data.length; i++) {
-  const title = document.createElement('h2');
+        const title = document.createElement('h2');
+    titleetdescription.appendChild(title);
   title.innerHTML = data[i].titre;
 
   const paragraphe = document.createElement('p');
-  paragraphe.innerHTML = data[i].paragraphe;
-
-  titleetdescription.appendChild(title);
   titleetdescription.appendChild(paragraphe);
+  paragraphe.innerHTML = data[i].description;
 }
+    //console.log(data)
+}
+fetchdata()
+//    const title= document.createElement('h2');
+//    titleetdescription.appendChild(title)
+//     title.innerHTML=data[i].titre
+
+// const paragraphe=document.createElement('p')
+// titleetdescription.appendChild(paragraphe)
+// title.innerHTML=data[i].paragraphe
+  
+
+// for (let i = 0; i < data.length; i++) {
+//   const title = document.createElement('h2');
+//     titleetdescription.appendChild(title);
+//   title.innerHTML = data[i].titre;
+
+//   const paragraphe = document.createElement('p');
+//   titleetdescription.appendChild(paragraphe);
+//   paragraphe.innerHTML = data[i].paragraphe;
+
+
+  
+// }
